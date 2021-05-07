a47dc34eecb5b6207c2e2c175f023c37536f2674
Mark J. Mononutu <s21810053@student.unklab.ac.id>
2021-05-07 08:10:37 +0800

    adding grid with input and table

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { NavBar } from "../../../components";
+import { Input } from "../../atoms";
 
 const Dashboard = () => {
   return (
-    <div className="">
+    <div style={{ backgroundColor: "bisque", height: "100vh" }}>
       <NavBar />
-      <div style={{ backgroundColor: "bisque", height: "100vh" }}>
-        <div className="container">
-          <h1>Test</h1>
+      <div className="container">
+        <h3 className="text-center mb-5">DASHBOARD</h3>
+        <hr />
+        <div className="row">
+          <div className="col-md-4">
+            <Input
+              label="Text1"
+              placeholder="placeholder"
+              className="form-control"
+            />
+            <Input
+              label="Text2"
+              placeholder="placeholder"
+              className="form-control"
+            />
+          </div>
+          <div className="col-sm ms-5">
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Column1</th>
+                  <th>Column2</th>
+                  <th>Column3</th>
+                  <th>Column4</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
         </div>
       </div>
     </div>
